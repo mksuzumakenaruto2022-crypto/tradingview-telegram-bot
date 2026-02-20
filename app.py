99abb4b0be14e0856fcb98e5e3243b6a58938afa
mksuzumakenaruto2022-crypto <mks.uzumakenaruto2022@gmail.com>
2026-02-20 11:50:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import os
 
 app = Flask(__name__)
 
+# Variáveis de ambiente (configuradas na Render)
 TELEGRAM_TOKEN = os.environ.get("BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.environ.get("CHAT_ID")
 
-
+# Função para enviar mensagem ao Telegram
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
@@ -16,32 +17,34 @@ def send_telegram_message(message):
     }
     requests.post(url, json=payload)
 
+# Rota principal (teste online)
+@app.route("/")
+def home():
+    return "Bot Online 🚀"
+
+# Webhook que recebe alerta do TradingView
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    
+
     symbol = data.get("symbol", "N/A")
     direction = data.get("direction", "N/A")
     timeframe = data.get("timeframe", "N/A")
 
     message = f"""
-🚨 NOVO SINAL 🚨
+📊 NOVO SINAL
 
-📊 Ativo: {symbol}
-⏰ Timeframe: {timeframe}
+📌 Ativo: {symbol}
+⏱ Timeframe: {timeframe}
 📈 Direção: {direction}
 
-Boa sorte 🍀
+Boa sorte 🚀
 """
 
     send_telegram_message(message)
-    return {"status": "ok"}
 
-@app.route("/")
-def home():
-    return "Bot Online 🚀"
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
